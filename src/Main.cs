95711de731f45518122f9a207ddd742b0dd8f553
t0stiman <18124323+t0stiman@users.noreply.github.com>
2023-10-30 19:42:20 +0100

    initial commit

---
+++ src/Main.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+using UnityModManagerNet;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace dv_f_spammer
+{
+    [EnableReloading]
+    static class Main
+    {
+        public static bool enabled;
+
+        private static InputSimulator inputSim;
+        private static DateTime previousKeyPressTime = new DateTime(0);
+
+        //================================================================
+
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnToggle = OnToggle;
+            modEntry.OnUpdate = OnUpdate;
+
+            modEntry.Logger.Log("loaded");
+            
+            inputSim = new InputSimulator();
+
+            return true;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value) 
+        {
+            enabled = value;
+            string msg = enabled ? "hello!" : "goodbye!";
+            modEntry.Logger.Log(msg);
+
+            return true;
+        }
+
+        private static void OnUpdate(UnityModManager.ModEntry modEntry, float idk)
+        {
+            bool getKey = Input.GetKey(KeyCode.F);
+            if (!getKey)
+            {
+                return;
+            }
+
+            if ((DateTime.UtcNow - previousKeyPressTime).TotalMilliseconds < 50)
+            {
+                return;
+            }
+
+            inputSim.Keyboard.KeyPress(VirtualKeyCode.VK_F);
+            previousKeyPressTime = DateTime.UtcNow;
+        }
+    }
+}
